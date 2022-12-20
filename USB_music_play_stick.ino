60d6d6b188c3943ee765a93ab558d5d24f746498
Huisu Lim <4922865@gmail.com>
2022-12-21 00:25:29 +0900

    complete coding of first play

--- USB_music_play_stick.ino
+++ USB_music_play_stick.ino
@@ -1,8 +1,17 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-void setup(){
+SoftwareSerial PlayerSerial(10,11);
+DFRobotDFPlayerMini Player;
 
+void setup(){
+  PlayerSerial.begin(9600);
+  Serial.begin(9600);
+  if(!Player.begin(PlayerSerial)){//connect to serial
+    while(true);
+  }
+  Player.volume(30);//max volume;
+  Player.play(1);//play first track;
 }
 
 void loop(){
